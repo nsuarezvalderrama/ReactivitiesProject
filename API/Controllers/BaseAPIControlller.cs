1f4736007068a945bc69f7bed50c948dd8bd0784
Norlys <nsuarezvalderrama@gmail.com>
2023-09-27 23:32:23 -0400

    Add Thin Controllers

--- API/Controllers/BaseAPIControlller.cs
+++ API/Controllers/BaseAPIControlller.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -6,5 +7,7 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class BaseAPIController : ControllerBase
 {
-    
+    public IMediator _mediator;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 }
